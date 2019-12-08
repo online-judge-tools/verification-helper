c8eb9335f7d64c1b15fdf6f8f8c7169203a70078
Tsutajiro <y.sugie.15739d@gmail.com>
2019-12-09 02:02:07 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='online-judge-verify-helper',
-    version='2.2.0',
+    version='2.2.1',
     author='Kimiyuki Onaka',
     author_email='kimiyuki95@gmail.com',
     url='https://github.com/kmyk/online-judge-verify-helper',

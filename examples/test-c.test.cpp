3dc7468b76d75ad14c214acb0761abf4289de17d
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-12-13 18:35:14 +0900

    Revert "test"
    This reverts commit 109627e221598bc6588d2fee15bbea3360988f05.

--- examples/test-c.test.cpp
+++
@@ -1,6 +0,0 @@
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A"
-#include <cstdio>
-
-int main() {
-    return 1;
-}
--- examples/test-d.test.cpp
+++
@@ -1,7 +0,0 @@
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A"
-#include <cstdio>
-
-int main() {
-    printf("Hello World\n");
-    return 0;
-}

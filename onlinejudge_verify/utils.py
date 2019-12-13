7d7775b6cf54f96123bc12e14c3bf10a0db55052
beet-aizu <aki.bdash@gmail.com>
2019-12-13 16:04:40 +0900

    ugokutoiina

--- onlinejudge_verify/utils.py
+++ onlinejudge_verify/utils.py
@@ -1,4 +1,5 @@
 # Python Version: 3.x
+import json
 import os
 import pathlib
 import shlex
@@ -7,6 +8,12 @@ from typing import *
 
 CXXFLAGS = os.environ.get('CXXFLAGS', '--std=c++17 -O2 -Wall -g')
 
+timestamps = {'~': 'dummy'}
+
+path_timestamp = pathlib.Path('.verify-helper/timestamp/timestamp.json')
+if path_timestamp.exists():
+    with open(path_timestamp, 'r') as f:
+        timestamps = json.loads(f.read())
 
 def list_depending_files(path: pathlib.Path, *, compiler: str) -> List[pathlib.Path]:
     code = r"""{} {} -I . -MD -MF /dev/stdout -MM {} | sed '1s/[^:].*: // ; s/\\$//' | xargs -n 1""".format(compiler, CXXFLAGS, shlex.quote(str(path)))
@@ -29,3 +36,16 @@ def get_last_commit_time_to_verify(path: pathlib.Path, *, compiler: str) -> str:
     depending_files = list_depending_files(path, compiler=compiler)
     code = ['git', 'log', '-1', '--date=iso', '--pretty=%ad', '--'] + list(map(lambda x: shlex.quote(str(x)), depending_files))
     return subprocess.check_output(code).decode().strip()
+
+
+def is_verified(path: pathlib.Path, compiler: str) -> bool:
+    return get_last_commit_time_to_verify(path, compiler=compiler) == timestamps.get(str(path), None)
+
+
+def mark_verified(path: pathlib.Path, compiler: str):
+    timestamps[str(path)] = get_last_commit_time_to_verify(path, compiler=compiler)
+
+
+def save_timestamps():
+    with open(path_timestamp, 'w') as f:
+        json.dump(timestamps, f, sort_keys=True, indent=0)
--- onlinejudge_verify/verify.py
+++ onlinejudge_verify/verify.py
@@ -1,22 +1,23 @@
 # Python Version: 3.x
+import glob
+import hashlib
 import math
 import os
 import pathlib
 import resource
+import shlex
 import subprocess
-import sys
 import tempfile
 import time
 from logging import getLogger
 from typing import *
 
-import pkg_resources
-
-package = 'onlinejudge_verify.data'
-bash_script = pkg_resources.resource_string(package, 'test.sh')
+import onlinejudge_verify.utils as utils
 
 logger = getLogger(__name__)
 
+def exec_command(command: List[str]):
+    subprocess.check_call(shlex.split(' '.join(command)))
 
 def main(paths: List[pathlib.Path], *, timeout: float = math.inf) -> None:
     try:
@@ -24,17 +25,94 @@ def main(paths: List[pathlib.Path], *, timeout: float = math.inf) -> None:
     except OSError:
         logger.warning('failed to make the stack size unlimited')
 
-    script = tempfile.NamedTemporaryFile(delete=False)
-    script.write(bash_script)
-    script.close()
-    try:
-        start = time.time()
-        for path in paths:
-            logger.info('verify %s', path)
-            subprocess.check_call(['/bin/bash', script.name, path], stdout=sys.stdout, stderr=sys.stderr)
+    compilers = []
+    if 'CXX' in os.environ:
+        compilers.append(os.environ.get('CXX'))
+    else:
+        compilers.append('g++')
+        compilers.append('clang++')
 
-            # to prevent taking too long; we may fail to use the results of verification due to expired tokens
-            if timeout is not None and time.time() - start > timeout:
-                break
-    finally:
-        os.remove(script.name)
+    cxxflags = os.environ.get('CXXFLAGS', '-std=c++17 -O2 -Wall -g')
+
+    start = time.time()
+    for path in paths:
+        if utils.is_verified(path, compiler=compilers[0]):
+            continue
+
+        verified = False
+        logger.info('verify %s', path)
+        for cxx in compilers:
+            macros = utils.list_defined_macros(path, compiler=cxx)
+
+            assert ('PROBLEM' in macros)
+            url = shlex.split(macros['PROBLEM'])[0]
+            directory = pathlib.Path('.verify-helper') / hashlib.md5(
+                url.encode()).hexdigest()
+
+            print(url)
+            if 'IGNORE' in macros:
+                continue
+
+            if not directory.exists():
+                directory.mkdir()
+                exec_command(['sleep', '2'])
+                exec_command([
+                    'oj', 'download', '--system', url, '-d',
+                    str(directory / 'test')
+                ])
+
+            print("$ $CXX $CXXFLAGS -I . $file")
+            exec_command([
+                cxx, cxxflags, '-I', '.', '-o',
+                str(directory / 'a.out'),
+                str(path)
+            ])
+
+            if 'judge.yosupo.jp' in url:
+                checker = onlinejudge.dispatch.problem_from_url(
+                    url).download_checker_cpp()
+                with open(directory / "checker.cpp", "wb") as f:
+                    f.write(checker)
+
+                with open(directory / 'testlib.h', 'w') as f:
+                    subprocess.call([
+                        'curl',
+                        'https://raw.githubusercontent.com/MikeMirzayanov/testlib/master/testlib.h'
+                    ],
+                                    stdout=f)
+
+                exec_command([
+                    cxx, cxxflags, '-I', '.', '-o',
+                    str(directory / 'checker.out'),
+                    str(directory / 'checker.cpp')
+                ])
+
+                exec_command([
+                    'oj', 'test', '--judge-command',
+                    str(directory / 'checker.out'), '-c',
+                    str(directory / 'a.out'), '-d',
+                    str(directory / 'test')
+                ])
+            elif 'ERROR' in macros:
+                error = shlex.split(macros['ERROR'])[0]
+                exec_command([
+                    'oj', 'test', '-e', error, '-c',
+                    str(directory / 'a.out'), '-d',
+                    str(directory / 'test')
+                ])
+            else:
+                exec_command([
+                    'oj', 'test', '-c',
+                    str(directory / 'a.out'), '-d',
+                    str(directory / 'test')
+                ])
+            verified = True
+
+        if verified:
+            utils.mark_verified(path, compilers[0])
+
+        # to prevent taking too long; we may fail to use the results of verification due to expired tokens
+        if timeout is not None and time.time() - start > timeout:
+            break
+
+    utils.save_timestamps()

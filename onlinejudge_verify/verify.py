ea6719aaefb5990f0a969834ac5a532db1349920
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-12-13 16:40:50 +0900

    fix format

--- onlinejudge_verify/verify.py
+++ onlinejudge_verify/verify.py
@@ -10,13 +10,16 @@ import time
 from logging import getLogger
 from typing import *
 
-import onlinejudge
 import onlinejudge_verify.utils as utils
+
+import onlinejudge
 
 logger = getLogger(__name__)
 
+
 def exec_command(command: List[str]):
     subprocess.check_call(shlex.split(' '.join(command)))
+
 
 def main(paths: List[pathlib.Path], *, timeout: float = math.inf) -> None:
     try:
@@ -26,7 +29,7 @@ def main(paths: List[pathlib.Path], *, timeout: float = math.inf) -> None:
 
     compilers = []
     if 'CXX' in os.environ:
-        compilers.append(os.environ.get('CXX'))
+        compilers.append(os.environ['CXX'])
     else:
         compilers.append('g++')
         compilers.append('clang++')
@@ -48,62 +51,32 @@ def main(paths: List[pathlib.Path], *, timeout: float = math.inf) -> None:
 
             assert ('PROBLEM' in macros)
             url = shlex.split(macros['PROBLEM'])[0]
-            directory = pathlib.Path('.verify-helper') / hashlib.md5(
-                url.encode()).hexdigest()
+            directory = pathlib.Path('.verify-helper') / hashlib.md5(url.encode()).hexdigest()
 
             if not directory.exists():
                 directory.mkdir()
                 exec_command(['sleep', '2'])
-                exec_command([
-                    'oj', 'download', '--system', url, '-d',
-                    str(directory / 'test')
-                ])
+                exec_command(['oj', 'download', '--system', url, '-d', str(directory / 'test')])
 
             print("$ $CXX $CXXFLAGS -I . $file")
-            exec_command([
-                cxx, cxxflags, '-I', '.', '-o',
-                str(directory / 'a.out'),
-                str(path)
-            ])
+            exec_command([cxx, cxxflags, '-I', '.', '-o', str(directory / 'a.out'), str(path)])
 
             if 'judge.yosupo.jp' in url:
-                checker = onlinejudge.dispatch.problem_from_url(
-                    url).download_checker_cpp()
+                checker = onlinejudge.dispatch.problem_from_url(url).download_checker_cpp()
                 with open(directory / "checker.cpp", "wb") as f:
                     f.write(checker)
 
-                with open(directory / 'testlib.h', 'w') as f:
-                    subprocess.call([
-                        'curl',
-                        'https://raw.githubusercontent.com/MikeMirzayanov/testlib/master/testlib.h'
-                    ],
-                                    stdout=f)
+                with open(directory / 'testlib.h', 'wb') as f:
+                    subprocess.call(['curl', 'https://raw.githubusercontent.com/MikeMirzayanov/testlib/master/testlib.h'], stdout=f)
 
-                exec_command([
-                    cxx, cxxflags, '-I', '.', '-o',
-                    str(directory / 'checker.out'),
-                    str(directory / 'checker.cpp')
-                ])
+                exec_command([cxx, cxxflags, '-I', '.', '-o', str(directory / 'checker.out'), str(directory / 'checker.cpp')])
 
-                exec_command([
-                    'oj', 'test', '--judge-command',
-                    str(directory / 'checker.out'), '-c',
-                    str(directory / 'a.out'), '-d',
-                    str(directory / 'test')
-                ])
+                exec_command(['oj', 'test', '--judge-command', str(directory / 'checker.out'), '-c', str(directory / 'a.out'), '-d', str(directory / 'test')])
             elif 'ERROR' in macros:
                 error = shlex.split(macros['ERROR'])[0]
-                exec_command([
-                    'oj', 'test', '-e', error, '-c',
-                    str(directory / 'a.out'), '-d',
-                    str(directory / 'test')
-                ])
+                exec_command(['oj', 'test', '-e', error, '-c', str(directory / 'a.out'), '-d', str(directory / 'test')])
             else:
-                exec_command([
-                    'oj', 'test', '-c',
-                    str(directory / 'a.out'), '-d',
-                    str(directory / 'test')
-                ])
+                exec_command(['oj', 'test', '-c', str(directory / 'a.out'), '-d', str(directory / 'test')])
             verified = True
 
         if verified:

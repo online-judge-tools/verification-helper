46f3c107703d6dcd45893f895b5a38ef6912f1b9
sash <sash2104@gmail.com>
2020-01-29 00:56:43 +0900

    add test for bundle.py

---
+++ tests/test_bundle.py
@@ -0,0 +1,21 @@
+import pathlib
+import unittest
+
+import tests.utils
+
+import onlinejudge_verify.bundle as bundle
+
+
+class TestStringMethods(unittest.TestCase):
+    def test_no_newline(self) -> None:
+        # 末尾に改行がないコードをincludeした時に改行が足されていることの確認
+        files = {
+            'no_newline.cpp': b'void foo() {}',
+            'example.test.cpp': b'#include "no_newline.cpp"'
+        }
+        path = pathlib.Path('example.test.cpp')
+        with tests.utils.load_files(files) as tempdir:
+            with tests.utils.chdir(tempdir):
+                bundler = bundle.Bundler(iquotes=[tempdir])
+                bundler.update(path)
+                self.assertIn(b'void foo() {}\n', bundler.get())

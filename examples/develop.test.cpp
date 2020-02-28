0ac85e279c235904e3e77407b7df59d438600a45
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-02-28 16:03:35 +0900

    Remove develop.test.cpp

--- examples/develop.test.cpp
+++
@@ -1,9 +0,0 @@
-// これは online-judge-verify-helper 本体のテスト用のファイルです
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A"
-#include <cstdio>
-
-int main() {
-    printf("Hello World\n");
-    return 0;
-}
-// hoge

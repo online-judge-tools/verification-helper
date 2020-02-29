99c4337f8876e69bb282f0cf3069aac42b93078e
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-02-29 19:42:09 +0900

    Revert "Add the suffix field for config.toml"
    This reverts commit 437563c8ece059d9b024b6c42fb39bbbc0232e83.

--- onlinejudge_verify/languages/__init__.py
+++ onlinejudge_verify/languages/__init__.py
@@ -12,21 +12,16 @@ logger = getLogger(__name__)
 
 _dict: Dict[str, Language] = {}
 
-_dict['.test.cpp'] = CPlusPlusLanguage()
-_dict['.test.hpp'] = _dict['.test.cpp']
-_dict['.test.csx'] = CSharpScriptLanguage()
+_dict['.cpp'] = CPlusPlusLanguage()
+_dict['.hpp'] = _dict['.cpp']
+_dict['.csx'] = CSharpScriptLanguage()
 
 config_path = pathlib.Path('.verify-helper/config.toml')
 if config_path.exists():
     for ext, config in toml.load(str(config_path)).get('languages', {}).items():
         logger.warn("%s: languages.%s: Adding new languages using `config.toml` is supported but not recommended. Please consider making pull requests for your languages, see https://github.com/kmyk/online-judge-verify-helper/issues/116", str(config_path), ext)
-        suffix = config.get('suffix', '.test.' + ext)
-        _dict[suffix] = OtherLanguage(config=config)
+        _dict['.' + ext] = OtherLanguage(config=config)
 
 
 def get(path: pathlib.Path) -> Optional[Language]:
-    for suffix, language in _dict.items():
-        if str(path.name).endswith(suffix):
-            return language
-    else:
-        return None
+    return _dict.get(path.suffix)

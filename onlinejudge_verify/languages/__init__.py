7365e34df5ac733db97aa244c3445bbb3d2c4e0b
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-03-17 12:59:54 +0900

    Add --config-file option

--- onlinejudge_verify/languages/__init__.py
+++ onlinejudge_verify/languages/__init__.py
@@ -3,7 +3,7 @@ from logging import getLogger
 from typing import *
 
 import toml
-from onlinejudge_verify.config import config_path, get_config
+from onlinejudge_verify.config import get_config
 from onlinejudge_verify.languages.cplusplus import CPlusPlusLanguage
 from onlinejudge_verify.languages.csharpscript import CSharpScriptLanguage
 from onlinejudge_verify.languages.models import Language, LanguageEnvironment
@@ -11,21 +11,27 @@ from onlinejudge_verify.languages.other import OtherLanguage
 
 logger = getLogger(__name__)
 
-_dict: Dict[str, Language] = {}
+_dict: Optional[Dict[str, Language]] = None
 
-_dict['.cpp'] = CPlusPlusLanguage()
-_dict['.hpp'] = _dict['.cpp']
-_dict['.csx'] = CSharpScriptLanguage()
 
-for ext, config in get_config().get('languages', {}).items():
-    logger.warn("%s: languages.%s: Adding new languages using `config.toml` is supported but not recommended. Please consider making pull requests for your languages, see https://github.com/kmyk/online-judge-verify-helper/issues/116", str(config_path), ext)
-    if '.' + ext in _dict:
-        for key in ('compile', 'execute', 'bundle', 'list_attributes', 'list_dependencies'):
-            if key in config:
-                raise RuntimeError("You cannot overwrite existing language: .{}".format(ext))
-    else:
-        _dict['.' + ext] = OtherLanguage(config=config)
+def _get_dict() -> Dict[str, Language]:
+    global _dict
+    if _dict is None:
+        _dict = {}
+        _dict['.cpp'] = CPlusPlusLanguage()
+        _dict['.hpp'] = _dict['.cpp']
+        _dict['.csx'] = CSharpScriptLanguage()
+
+        for ext, config in get_config().get('languages', {}).items():
+            logger.warn("config.toml: languages.%s: Adding new languages using `config.toml` is supported but not recommended. Please consider making pull requests for your languages, see https://github.com/kmyk/online-judge-verify-helper/issues/116", ext)
+            if '.' + ext in _dict:
+                for key in ('compile', 'execute', 'bundle', 'list_attributes', 'list_dependencies'):
+                    if key in config:
+                        raise RuntimeError("You cannot overwrite existing language: .{}".format(ext))
+            else:
+                _dict['.' + ext] = OtherLanguage(config=config)
+    return _dict
 
 
 def get(path: pathlib.Path) -> Optional[Language]:
-    return _dict.get(path.suffix)
+    return _get_dict().get(path.suffix)

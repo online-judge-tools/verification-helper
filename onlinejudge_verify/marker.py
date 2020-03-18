b0b12dc53d282c566ad21729ca0bdb5e01d14377
keymoon <kymn0116@gmail.com>
2020-03-18 22:57:02 +0900

    add cast for iterator

--- onlinejudge_verify/marker.py
+++ onlinejudge_verify/marker.py
@@ -126,7 +126,7 @@ def _get_last_commit_time_to_verify(path: pathlib.Path) -> datetime.datetime:
     language = onlinejudge_verify.languages.get(path)
     assert language is not None
     depending_files = language.list_dependencies(path, basedir=pathlib.Path.cwd())
-    code = ['git', 'log', '-1', '--date=iso', '--pretty=%ad', '--'] + map(str, depending_files)
+    code = ['git', 'log', '-1', '--date=iso', '--pretty=%ad', '--'] + list(map(str, depending_files))
     timestamp = subprocess.check_output(code).decode().strip()
     if not timestamp:
         return datetime.datetime.fromtimestamp(0, tz=datetime.timezone(datetime.timedelta()))

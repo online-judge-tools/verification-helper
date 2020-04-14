dd73d9a10ade4ba08eb1a1cedd8b2b399a4c9084
chaemon <chaemon@gmail.com>
2020-04-15 00:41:55 +0900

    sedを使わないように変更。無駄なsplitとjoinを削除。

--- onlinejudge_verify/languages/nim.py
+++ onlinejudge_verify/languages/nim.py
@@ -1,5 +1,6 @@
 # Python Version: 3.x
 import pathlib
+import re
 import shlex
 import subprocess
 from logging import getLogger
@@ -20,9 +21,9 @@ class NimLanguageEnvironment(LanguageEnvironment):
         self.compile_to = compile_to
 
     def compile(self, path: pathlib.Path, *, basedir: pathlib.Path, tempdir: pathlib.Path) -> None:
-        command = compile = ["nim", self.compile_to, "-p:.", "-d:release", "-o:{tempdir}/a.out".format(tempdir=str(tempdir)), ' '.join(self.NIMFLAGS), "{path}".format(path=str(path))]
+        command = ["nim", self.compile_to, "-p:.", "-d:release", "-o:{tempdir}/a.out".format(tempdir=str(tempdir)), ' '.join(self.NIMFLAGS), "{path}".format(path=str(path))]
         logger.info('$ %s', ' '.join(command))
-        subprocess.check_call(shlex.split(command))
+        subprocess.check_call(command)
 
     def get_execute_command(self, path: pathlib.Path, *, basedir: pathlib.Path, tempdir: pathlib.Path) -> List[str]:
         command = execute = "{tempdir}/a.out".format(path=str(path), basedir=str(basedir), tempdir=str(tempdir))
@@ -39,12 +40,16 @@ class NimLanguage(Language):
             self.config = config
 
     def list_dependencies(self, path: pathlib.Path, *, basedir: pathlib.Path) -> List[pathlib.Path]:
-        command = "sed 's/^include \"\\(.*\\)\"$/\\1/ ; t ; d' {path}".format(path=str(path), basedir=str(basedir))
-        text = subprocess.check_output(shlex.split(command))
-        dependencies = [path]
-        for line in text.splitlines():
-            print(line)
-            dependencies.append(pathlib.Path(line.decode()))
+        texts = []
+        with open("{basedir}/{path}".format(path=str(path), basedir=str(basedir)), 'r') as f:
+            pattern = re.compile(r"include\s*\"(.*)\"")
+            for line in f:
+                line = line.strip()
+                if line.startswith('include'):
+                    texts += re.findall(pattern, line)
+        dependencies = []
+        for line in texts:
+            dependencies.append(pathlib.Path(line))
         return dependencies
 
     def bundle(self, path: pathlib.Path, *, basedir: pathlib.Path) -> bytes:
@@ -55,8 +60,8 @@ class NimLanguage(Language):
 
     def list_environments(self, path: pathlib.Path, *, basedir: pathlib.Path) -> Sequence[NimLanguageEnvironment]:
         compile_to = "cpp"
-        if "compileTo" in self.config:
-            compile_to = self.config["compileTo"]
+        if "compile_to" in self.config:
+            compile_to = self.config["compile_to"]
         NIMFLAGS = ""
         if "NIMFLAGS" in self.config:
             NIMFLAGS = self.config["NIMFLAGS"].split()

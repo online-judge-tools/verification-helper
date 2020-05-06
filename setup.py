76447d2c52596c727c1c72f2772f3bc1e8bd010b
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-05-07 03:41:36 +0900

    RELEASE: 4.10.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='online-judge-verify-helper',
-    version='4.10.0',
+    version='4.10.1',
     author='Kimiyuki Onaka',
     author_email='kimiyuki95@gmail.com',
     url='https://github.com/kmyk/online-judge-verify-helper',

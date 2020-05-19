9f4ebc7b3fe26d78aa9acfda5a64da48ec569268
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-05-20 02:59:19 +0900

    Format tests/test_bundle.py

--- tests/test_bundle.py
+++ tests/test_bundle.py
@@ -1,4 +1,5 @@
 import pathlib
+import textwrap
 import unittest
 
 import onlinejudge_verify.languages.cplusplus_bundle as cplusplus_bundle
@@ -8,7 +9,14 @@ import tests.utils
 class TestStringMethods(unittest.TestCase):
     def test_no_newline(self) -> None:
         # 末尾に改行がないコードをincludeした時に改行が足されていることの確認
-        files = {'no_newline.cpp': b'void foo() {}', 'example.test.cpp': b'#include "no_newline.cpp"\n#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_B"'}
+
+        files = {
+            'no_newline.cpp': b'void foo() {}',
+            'example.test.cpp': textwrap.dedent("""\
+                #include "no_newline.cpp"
+                #define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_B"
+                """).encode(),
+        }
         path = pathlib.Path('example.test.cpp')
         with tests.utils.load_files(files) as tempdir:
             with tests.utils.chdir(tempdir):

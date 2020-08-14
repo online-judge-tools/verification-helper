f828b11615164c32b573d7d216c1b94eb588ecb9
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-08-15 03:14:24 +0900

    Add end-to-end tests for oj-bundle command

--- tests/test_bundle.py
+++ tests/test_bundle.py
@@ -1,19 +1,24 @@
 """This module has tests for the bundle feature.
 """
 
+import contextlib
 import pathlib
 import platform
 import shutil
+import subprocess
+import sys
+import tempfile
 import textwrap
 import unittest
 
+import onlinejudge_bundle.main
 import onlinejudge_verify.languages.cplusplus_bundle as cplusplus_bundle
 import tests.utils
 from onlinejudge_verify.languages.cplusplus_bundle import BundleError
 
 
 @unittest.skipIf(platform.system() == 'Darwin', 'We cannot use the fake g++ of macOS.')
-class TestCPlusPlusBundling(unittest.TestCase):
+class TestCPlusPlusBundlingUnit(unittest.TestCase):
     def test_no_newline(self) -> None:
         # 末尾に改行がないコードをincludeした時に改行が足されていることの確認
 
@@ -69,3 +74,104 @@ class TestCPlusPlusBundling(unittest.TestCase):
             with tests.utils.chdir(tempdir):
                 bundler = cplusplus_bundle.Bundler(iquotes=[tempdir], compiler='clang++')
                 self.assertRaises(BundleError, lambda: bundler.update(path))
+
+
+@unittest.skipIf(platform.system() == 'Darwin', 'We cannot use the fake g++ of macOS.')
+class TestCPlusPlusBundlingEndToEnd(unittest.TestCase):
+    def test_smoke(self) -> None:
+        files = {
+            'library.hpp': textwrap.dedent("""\
+                #pragma once
+                #include <string>
+
+                std::string get_hello_world() {
+                    return "Hello World";
+                }
+                """).encode(),
+            'main.cpp': textwrap.dedent("""\
+                #define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_A"
+                #include <iostream>
+                #include "library.hpp"
+                using namespace std;
+
+                int main() {
+                    cout << get_hello_world() << endl;
+                    return 0;
+                }
+                """).encode(),
+        }
+
+        with tests.utils.load_files(files) as tempdir:
+            with tests.utils.chdir(tempdir):
+                # bundle
+                with open('main.bundled.cpp', 'w') as fh:
+                    with contextlib.redirect_stdout(fh):
+                        onlinejudge_bundle.main.main(args=['main.cpp'])
+
+                # compile
+                subprocess.check_call(['g++', 'main.bundled.cpp'], stderr=sys.stderr)
+
+                # run
+                self.assertEqual(subprocess.check_output(['./a.out']), b'Hello World\n')
+
+    def test_complicated(self) -> None:
+        library_files = {
+            pathlib.Path('library', 'macro.hpp'): textwrap.dedent("""\
+                #pragma once
+                #define REP(i, n) for (int i = 0; (i) < (int)(n); ++ (i))
+                #define REP3(i, m, n) for (int i = (m); (i) < (int)(n); ++ (i))
+                #define REP_R(i, n) for (int i = (int)(n) - 1; (i) >= 0; -- (i))
+                #define REP3R(i, m, n) for (int i = (int)(n) - 1; (i) >= (int)(m); -- (i))
+                #define ALL(x) std::begin(x), std::end(x)
+                """).encode(),
+            pathlib.Path('library', 'fibonacci.hpp'): textwrap.dedent("""\
+                #pragma once
+                #include <cstdint>
+                #include <vector>
+                #include "library/macro.hpp"
+
+                int64_t calculate_fibonacci(int n) {
+                    std::vector<int64_t> dp(n + 1);
+                    dp[0] = 0;
+                    dp[1] = 1;
+                    REP3 (i, 2, n + 1) {
+                        dp[i] = dp[i - 1] + dp[i - 2];
+                    }
+                    return dp[n];
+                }
+                """).encode(),
+        }
+
+        test_files = {
+            pathlib.Path('test', 'main.cpp'): textwrap.dedent("""\
+                #include <iostream>
+                #include "library/macro.hpp"
+                #include "library/fibonacci.hpp"
+                using namespace std;
+
+                int main() {
+                    int n; cin >> n;
+                    cout << calculate_fibonacci(n) << endl;
+                    return 0;
+                }
+                """).encode(),
+        }
+
+        with tempfile.TemporaryDirectory() as tempdir_dst_:
+            tempdir_dst = pathlib.Path(tempdir_dst_)
+
+            # bundle
+            with tests.utils.load_files_pathlib(library_files) as tempdir_src_library:
+                with tests.utils.load_files_pathlib(test_files) as tempdir_src_test:
+                    with open(tempdir_dst / 'main.bundled.cpp', 'w') as fh:
+                        with contextlib.redirect_stdout(fh):
+                            args = ['-I', str(tempdir_src_library), str(tempdir_src_test / 'test' / 'main.cpp')]
+                            onlinejudge_bundle.main.main(args=args)
+
+            # compile
+            subprocess.check_call(['g++', '-o', str(tempdir_dst / 'a.out'), str(tempdir_dst / 'main.bundled.cpp')], stderr=sys.stderr)
+
+            # run
+            self.assertEqual(subprocess.check_output([str(tempdir_dst / 'a.out')], input=b'10\n'), b'55\n')
+            self.assertEqual(subprocess.check_output([str(tempdir_dst / 'a.out')], input=b'20\n'), b'6765\n')
+            self.assertEqual(subprocess.check_output([str(tempdir_dst / 'a.out')], input=b'30\n'), b'832040\n')

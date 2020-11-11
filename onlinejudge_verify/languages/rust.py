8f6c759280a3ee57e8fc09da1667c7cfaa1cfeab
Ryo Yamashita <qryxip@gmail.com>
2020-11-12 01:51:45 +0900

    Apply mypy, pylint, isrot, and yapf

--- onlinejudge_verify/languages/rust.py
+++ onlinejudge_verify/languages/rust.py
@@ -11,12 +11,10 @@ class RustLanguageEnvironment(LanguageEnvironment):
     def __init__(self):
         raise NotImplementedError
 
-    def compile(self, path: pathlib.Path, *, basedir: pathlib.Path,
-                tempdir: pathlib.Path) -> None:
+    def compile(self, path: pathlib.Path, *, basedir: pathlib.Path, tempdir: pathlib.Path) -> None:
         raise NotImplementedError
 
-    def get_execute_command(self, path: pathlib.Path, *, basedir: pathlib.Path,
-                            tempdir: pathlib.Path) -> List[str]:
+    def get_execute_command(self, path: pathlib.Path, *, basedir: pathlib.Path, tempdir: pathlib.Path) -> List[str]:
         raise NotImplementedError
 
 
@@ -24,14 +22,14 @@ class RustLanguage(Language):
     def __init__(self, *, config: Optional[Dict[str, Any]] = None):
         raise NotImplementedError
 
-    def list_dependencies(self, path: pathlib.Path, *,
-                          basedir: pathlib.Path) -> List[pathlib.Path]:
+    def list_dependencies(self, path: pathlib.Path, *, basedir: pathlib.Path) -> List[pathlib.Path]:
         raise NotImplementedError
 
-    def is_verification_file(self, path: pathlib.Path, *,
-                             basedir: pathlib.Path) -> bool:
+    def bundle(self, path: pathlib.Path, *, basedir: pathlib.Path, options: Dict[str, Any]) -> bytes:
         raise NotImplementedError
 
-    def list_environments(self, path: pathlib.Path, *, basedir: pathlib.Path
-                          ) -> Sequence[RustLanguageEnvironment]:
+    def is_verification_file(self, path: pathlib.Path, *, basedir: pathlib.Path) -> bool:
+        raise NotImplementedError
+
+    def list_environments(self, path: pathlib.Path, *, basedir: pathlib.Path) -> Sequence[RustLanguageEnvironment]:
         raise NotImplementedError
